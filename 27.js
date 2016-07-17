71fec0f0a93e9ec5a789dbad48206e414748e541
DJSnacks <ricardo.carrillo.266@gmail.com>
2016-07-17 14:49:14 -0700

    Add files via upload

---
+++ 27.js
@@ -0,0 +1,8 @@
+function Movie(title,rating,directors,producers,studio, releasedate) {
+    this.title = title
+    this.rating = rating
+    this.directors = directors
+    this.producers = producers
+    this.studio = studio
+	this.releasedate = releasedate
+}
